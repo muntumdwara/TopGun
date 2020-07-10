dd7b69a07ec6095d85891cf9a12b3b6087608470
djmcnay <39102979+djmcnay@users.noreply.github.com>
2020-07-10 11:30:29 +0200

    Create __init__.py

---
+++ topgun/models/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+HongXiongMao
+
+@author: David
+
+"""
+
+# Useful Medium post on Python importing within __init__.py
+# https://towardsdatascience.com/whats-init-for-me-d70a312da583
