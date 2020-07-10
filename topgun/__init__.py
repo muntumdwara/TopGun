c3077c0cbf7ec43dc57cda240106b1a0dc20cdfc
djmcnay <39102979+djmcnay@users.noreply.github.com>
2020-07-10 11:21:10 +0200

    Create __init__.py

---
+++ topgun/__init__.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+HongXiongMao
+
+@author: David
+
+"""
+
+# Useful Medium post on Python importing within __init__.py
+# https://towardsdatascience.com/whats-init-for-me-d70a312da583
+
+# General
+import hongxiongmao.utilities
+import hongxiongmao.download
+import hongxiongmao.config
+
+# Plotting
+import hongxiongmao.overplot
+
+# Modelling
+import hongxiongmao.correlation
+import hongxiongmao.pokemon
+
+# Optimisation
+import hongxiongmao.optimisation
+
