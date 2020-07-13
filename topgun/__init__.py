9eb8906669fed347c81d25c988abc58598cff2aa
djmcnay <39102979+djmcnay@users.noreply.github.com>
2020-07-13 08:10:47 +0200

    Update __init__.py

--- topgun/__init__.py
+++ topgun/__init__.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-HongXiongMao
-
-@author: David
+TopGun
+@author: Mavrick
 
 """
 
 # Useful Medium post on Python importing within __init__.py
 # https://towardsdatascience.com/whats-init-for-me-d70a312da583
 
+### DON'T FORGET TO UPDATE SUB __INIT__.py files ###
+
 # General
 from topgun.utilities import *
 
-
+# Models
 from .models import *
