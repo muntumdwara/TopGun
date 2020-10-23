3b534108fc1fb8f1067ac062e20c4b5c0bb07bf2
djmcnay <39102979+djmcnay@users.noreply.github.com>
2020-10-23 10:42:05 +0200

    Update backtest.py
    Hiding test code

--- topgun/optimiser/backtest.py
+++ topgun/optimiser/backtest.py
@@ -1258,29 +1258,29 @@ class BacktestAnalytics(object):
 
 # %% TEST CODE
         
-import xlwings as xlw
+# import xlwings as xlw
 
-wb = xlw.Book('BACKTEST.xlsm')
+# wb = xlw.Book('BACKTEST.xlsm')
 
-# index data from timeseries sheet
-benchmarks = wb.sheets['TIMESERIES'].range('D1').options(pd.DataFrame, expand='table').value.iloc[3:,:]
-benchmarks.index = pd.to_datetime(benchmarks.index)
+# # index data from timeseries sheet
+# benchmarks = wb.sheets['TIMESERIES'].range('D1').options(pd.DataFrame, expand='table').value.iloc[3:,:]
+# benchmarks.index = pd.to_datetime(benchmarks.index)
 
-E = wb.sheets['Enhanced'].range('A1').options(pd.DataFrame, expand='table').value.iloc[:,1]
-C = wb.sheets['Core'].range('A1').options(pd.DataFrame, expand='table').value.iloc[:,1]
-E.index = E.index + pd.offsets.MonthEnd(0)
-C.index = C.index + pd.offsets.MonthEnd(0)
-E.name = 'Enhanced'
-C.name = 'Core'
+# E = wb.sheets['Enhanced'].range('A1').options(pd.DataFrame, expand='table').value.iloc[:,1]
+# C = wb.sheets['Core'].range('A1').options(pd.DataFrame, expand='table').value.iloc[:,1]
+# E.index = E.index + pd.offsets.MonthEnd(0)
+# C.index = C.index + pd.offsets.MonthEnd(0)
+# E.name = 'Enhanced'
+# C.name = 'Core'
 
-rtns = pd.concat([E, C], axis=1).dropna()
-x = 0.3
-rtns['E30'] = rtns['Enhanced'] * x + rtns['Core'] * (1 - x)
+# rtns = pd.concat([E, C], axis=1).dropna()
+# x = 0.3
+# rtns['E30'] = rtns['Enhanced'] * x + rtns['Core'] * (1 - x)
 
-bt = BacktestAnalytics(rtns, benchmarks, bmks_as_rtns=False, benchmark='SWIX', Rf='STEFI')
-md = bt.big_bang(title="TEST")
-from topgun.reporting import Reporting
-Reporting().md2html(md=md, title='test')
+# bt = BacktestAnalytics(rtns, benchmarks, bmks_as_rtns=False, benchmark='SWIX', Rf='STEFI')
+# md = bt.big_bang(title="TEST")
+# from topgun.reporting import Reporting
+# Reporting().md2html(md=md, title='test')
 
 #print(df)
 #x = bt.rolling
